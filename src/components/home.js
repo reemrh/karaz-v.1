cdef0431ee566f9d7b3a92ae671944b5e6c10587
reemrh <reemrmh97@gmail.com>
2020-03-26 16:24:54 +0200

    ii

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { Link ,Redirect} from "react-router-dom";
 function Home(props){
-    return <Link to="/admin">Admin</Link>
+    return (
+    <>
+      <Link to="/admin">Admin</Link>
+      <Link to="/settings">Settings</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/signup">Signup</Link>
+    </>
+    );
+
 }
 export default  Home;
