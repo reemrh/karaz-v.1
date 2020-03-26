0bae22703584d5b0858a88ab0ef8fb37dafd73d6
reemrh <reemrmh97@gmail.com>
2020-03-26 16:26:24 +0200

    ii

--- src/components/home.js
+++ src/components/home.js
@@ -3,11 +3,11 @@ import { Link ,Redirect} from "react-router-dom";
 function Home(props){
     return (
     <>
-      <Link to="/admin">Admin</Link>
-      <Link to="/settings">Settings</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/signup">Signup</Link>
+      <Link to="/admin">Admin</Link> <br/>
+      <Link to="/settings">Settings</Link><br/>
+      <Link to="/login">Login</Link><br/>
+      <Link to="/dashboard">Dashboard</Link><br/>
+      <Link to="/signup">Signup</Link><br/>
     </>
     );
 
